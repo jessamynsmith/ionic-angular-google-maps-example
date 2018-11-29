29051338c6593e90f7ec91e24a7b701fd0521990
jessamynsmith <jessamyn.smith@gmail.com>
2018-11-28 17:51:35 -0700

    This project's express server exists purely to serve the static front end. It does not need CORS set up.

--- server.js
+++ server.js
@@ -9,13 +9,6 @@ app.use(sslRedirect());
 
 app.use(express.static('www'));
 
-// CORS (Cross-Origin Resource Sharing) headers to support Cross-site HTTP requests
-app.all('*', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next();
-});
-
 app.set('port', process.env.PORT || 5000);
 
 app.listen(app.get('port'), function() {
